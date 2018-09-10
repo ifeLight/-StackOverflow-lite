e74f494ba260b5f80b9c87b4d1918ccf12ae3d95
Ifedayo Karim <karimjnr@gmail.com>
2018-09-10 16:17:09 +0100

    Test Database created

--- config/db.js
+++ config/db.js
@@ -7,7 +7,11 @@ dotenv.config();
  * Database Connection String
  * Example : postgresql://username:password@server:port/databaseName
  */
-const connectionString = process.env.DATABASE_URL || 'postgres://mdhiagiz:LwMBraBjyZWw87qf2YI1KVIj1bsqRQK6@pellefant.db.elephantsql.com:5432/mdhiagiz';
+let connectionString = process.env.DATABASE_URL || 'postgres://mdhiagiz:LwMBraBjyZWw87qf2YI1KVIj1bsqRQK6@pellefant.db.elephantsql.com:5432/mdhiagiz';
+
+if (process.env.NODE_ENV === 'test') {
+  connectionString = 'postgres://odwvbluu:JXUsZ8hk5tF5jijZrkS14T1ddK6_0R6h@stampy.db.elephantsql.com:5432/odwvbluu';
+}
 
 const pool = new Pool({
   connectionString,
